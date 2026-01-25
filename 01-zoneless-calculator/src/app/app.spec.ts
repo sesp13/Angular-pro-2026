8b46131b9267ffec876a5f4e0bbbbf6475c6cadc
sesp13 <sespinosarte99@gmail.com>
2026-01-24 20:43:12 -0500

    start testing for app component

--- 01-zoneless-calculator/src/app/app.spec.ts
+++ 01-zoneless-calculator/src/app/app.spec.ts
@@ -8,16 +8,34 @@ describe('App', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create app', () => {
     const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const appComponent = fixture.componentInstance;
+    // Access to the template
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    // Check component existence
+    expect(appComponent).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render router-outlet', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+    // Template
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
+
+  it('should render router-outlet with css classes', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses =
+      'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(
+        ' ',
+      );
+
+    divElement?.classList.forEach((className) => {
+      expect(mustHaveClasses).toContain(className);
+    });
   });
 });
