3984a5361947733fa1d55155ad9f2b7703094ce4
sesp13 <sespinosarte99@gmail.com>
2026-02-04 21:26:09 -0500

    add prerendered routes}

--- 02-pokemon-ssr/src/app/app.routes.server.ts
+++ 02-pokemon-ssr/src/app/app.routes.server.ts
@@ -1,8 +1,36 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+const POKEMON_LIMIT = 151;
+
+async function fetchPokemonNames(limit: number): Promise<string[]> {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}`,
+  );
+  const data = await response.json();
+  return data.results.map((pokemon: { name: string }) => pokemon.name);
+}
+
 export const serverRoutes: ServerRoute[] = [
+  // Static routes implementation
+  {
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return Array.from({ length: POKEMON_LIMIT }, (_, i) => ({
+        page: (i + 1).toString(),
+      }));
+    },
+  },
+  {
+    path: 'pokemon/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const names = await fetchPokemonNames(POKEMON_LIMIT);
+      return names.map(name => ({ id: name }));
+    },
+  },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
--- 02-pokemon-ssr/src/app/pages/pokemons-page/pokemons-page.ts
+++ 02-pokemon-ssr/src/app/pages/pokemons-page/pokemons-page.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  effect,
   inject,
   OnInit,
   signal,
@@ -9,18 +10,18 @@ import { PokemonList } from '../../pokemons/components/pokemon-list/pokemon-list
 import { PokemonListSkeleton } from './ui/pokemon-list-skeleton/pokemon-list-skeleton';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { SimplePokemon } from '../../pokemons/interfaces';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map, tap } from 'rxjs';
 import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-pokemons-page',
-  imports: [PokemonList, PokemonListSkeleton],
+  imports: [PokemonList, PokemonListSkeleton, RouterLink],
   templateUrl: './pokemons-page.html',
   styleUrl: './pokemons-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPage implements OnInit {
+export default class PokemonsPage {
   public pokemons = signal<SimplePokemon[]>([]);
   private pokemonService = inject(PokemonsService);
   private route = inject(ActivatedRoute);
@@ -28,29 +29,24 @@ export default class PokemonsPage implements OnInit {
   private title = inject(Title);
 
   public currentPage = toSignal(
-    this.route.queryParamMap.pipe(
-      map((params) => params.get('page') ?? '1'),
+    this.route.params.pipe(
+      map((params) => params['page'] ?? '1'),
       map((page) => (isNaN(+page) ? 1 : +page)),
       map((page) => Math.max(1, page)),
     ),
   );
 
-  ngOnInit(): void {
-    console.log(this.currentPage());
-    this.loadPokemons();
-  }
+  public loadOnPageChange = effect(() => {
+    const pageToLoad = this.currentPage();
+    this.loadPokemons(pageToLoad);
+  });
 
   public loadPokemons(page = 0) {
-    const pageToLoad = this.currentPage()! + page;
-
     this.pokemonService
-      .loadPage(pageToLoad)
+      .loadPage(page)
       .pipe(
         tap(() => {
-          this.router.navigate([], { queryParams: { page: pageToLoad } });
-        }),
-        tap(() => {
-          this.title.setTitle(`Pokemons SSR - Page ${pageToLoad}`);
+          this.title.setTitle(`Pokemons SSR - Page ${page}`);
         }),
       )
       .subscribe((pokemons) => {
