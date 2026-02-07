e3f6ff54c02f0b7b545f01aff5b8d55fd02df4cf
sesp13 <sespinosarte99@gmail.com>
2026-02-06 21:14:28 -0500

    new config

--- 02-pokemon-ssr/src/app/app.component.spec.ts
+++ 02-pokemon-ssr/src/app/app.component.spec.ts
@@ -1,23 +1,66 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
+import { Navbar } from './shared/components/navbar/navbar';
+import { describe, it, expect, beforeEach } from 'vitest';
+
+@Component({
+  selector: 'app-navbar',
+  template: `<nav>
+    <a href="test-link" class="test-class">Test link</a>
+  </nav>`,
+})
+class MockNavbarComponent {}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
+  // WAY Provide modules required by child components
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     imports: [AppComponent],
+  //     providers: [provideRouter([])]
+  //   }).compileComponents();
+  // });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+    })
+      .overrideComponent(AppComponent, {
+        add: {
+          imports: [MockNavbarComponent],
+        },
+        remove: {
+          imports: [Navbar],
+        },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'pokemon-ssr' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('pokemon-ssr');
   });
 
+  it('should render the navbar and the router outlet', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('app-navbar')).toBeTruthy();
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
+
+  it('should match snapshot', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.innerHTML).toMatchSnapshot();
+  });
 });
