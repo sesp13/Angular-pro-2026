463267119d068ab4001f810aa0f4fd019ad0873a
sesp13 <sespinosarte99@gmail.com>
2026-02-24 23:37:31 -0500

    add tests for my own package

--- 05-sesp-workspace/projects/my-sesp-sidebar/src/lib/my-sesp-sidebar.spec.ts
+++ 05-sesp-workspace/projects/my-sesp-sidebar/src/lib/my-sesp-sidebar.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MySespSidebar } from './my-sesp-sidebar';
+import { provideRouter } from '@angular/router';
 
 describe('MySespSidebar', () => {
   let component: MySespSidebar;
@@ -8,9 +9,9 @@ describe('MySespSidebar', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MySespSidebar]
-    })
-    .compileComponents();
+      imports: [MySespSidebar],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MySespSidebar);
     component = fixture.componentInstance;
@@ -19,5 +20,31 @@ describe('MySespSidebar', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSignIn when login button is clicked', () => {
+     vi.spyOn(component.onSignIn, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', false);
+    fixture.detectChanges();
+
+    const compiled = (fixture.nativeElement as HTMLElement);
+    const button = compiled.querySelector('[data-login]') as HTMLButtonElement;
+    button.click();
+
+    expect(component.onSignIn.emit).toHaveBeenCalled();
+
+  });
+
+  it('should call onSignOut when logout button is clicked', () => {
+     vi.spyOn(component.onSignOut, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', true);
+    fixture.detectChanges();
+
+    const compiled = (fixture.nativeElement as HTMLElement);
+    const button = compiled.querySelector('[data-logout]') as HTMLButtonElement;
+    button.click();
+
+    expect(component.onSignOut.emit).toHaveBeenCalled();
+
   });
 });
