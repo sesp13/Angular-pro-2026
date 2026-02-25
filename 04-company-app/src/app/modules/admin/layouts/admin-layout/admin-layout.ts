ac0a32051e99688197ba0e1dbff940c4c368f809
sesp13 <sespinosarte99@gmail.com>
2026-02-24 23:39:30 -0500

    use external custom package

--- 04-company-app/src/app/modules/admin/layouts/admin-layout/admin-layout.ts
+++ 04-company-app/src/app/modules/admin/layouts/admin-layout/admin-layout.ts
@@ -1,16 +1,18 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideMenu } from "../../../shared/components/side-menu/side-menu";
+import { ApxSideMenuComponent, TitleColor } from 'apx-side-menu';
 
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, SideMenu],
+  imports: [RouterOutlet, SideMenu, ApxSideMenuComponent],
   templateUrl: './admin-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayout { 
   isAutheticated = signal(false);
+  TitleColor = TitleColor;
 
   onLogin() {
     this.isAutheticated.set(true);
